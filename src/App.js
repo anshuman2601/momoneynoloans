087e393aaa33004133f5232f99f61369bde2674a
mcsilva19 <maria-silva@uiowa.edu>
2023-04-12 20:37:40 -0500

    hello guys

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          JB is the best student agree?
+          FuckJB  student agree?hfjyfjyffh
         </p>
         <a
           className="App-link"
