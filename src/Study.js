450e65179a3fa68732634d7fef93922f5c6270ff
JB Lee <jeongbeom98@gmail.com>
2023-04-16 14:47:16 -0500

    formatting

--- src/Study.js
+++ src/Study.js
@@ -5,9 +5,11 @@ function Study() {
   return (
     <div>
     <Navbar />
+    <main role="main" class="container">
+    <h1>This is Study Page</h1>
 
-    This is Study Page
-
+    
+    </main>
     </div>
    
 
