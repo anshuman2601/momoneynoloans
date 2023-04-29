274746cfb0cfd62f1d0eed13c05194fddb2936a6
JB Lee <jeongbeom98@gmail.com>
2023-04-29 18:13:59 -0500

    jb grind

--- src/Login.js
+++ src/Login.js
@@ -1,41 +1,74 @@
-// create a login & registration form using Bootstrap 5
-
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { useNavigate, Link } from 'react-router-dom';
 import Navbar from './Navbar';
+import Profile from './Profile';
+
 function Login() {
-    return (
-        <div className="container"> 
-        <Navbar />
-                <div className="row">
-                    <div className="col-md-6 mx-auto mt-5">
-                        <div className="card">
-                            <div className="card-header">
-                                <h1 className="h3 mb-0">Login</h1>
-                            </div>
-                            <div className="card-body">
-                                <form>
-                                    <div className="mb-3">
-                                        <label className="form-label" htmlFor="email">Email address</label>
-                                        <input type="email" className="form-control" name="email" id="email" aria-describedby="emailHelp" />
-                                    </div>
-                                    <div className="mb-3">
-                                        <label className="form-label" htmlFor="password">Password</label>
-                                        <input type="password" className="form-control" name="password" id="password" />
-                                    </div>
-                                    <button type="submit" className="btn btn-primary">Login</button>
-                                </form>
-                            </div>
-                            <div className="card-footer">
-                                <p className="text-muted">
-                                    Don't have an account? <Link to="/register" className="text-decoration-none">Register</Link>
-                                </p>
-                            </div>
-                        </div>
-                    </div>
+  const navigate = useNavigate();
+  const [email, setEmail] = useState('');
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    setEmail(e.target.email.value);
+    navigate('/profile', { state: { email: e.target.email.value } });
+  };
+
+  return (
+    <div className="container">
+      <Navbar />
+      <div className="row">
+        <div className="col-md-6 mx-auto mt-5">
+          <div className="card">
+            <div className="card-header">
+              <h1 className="h3 mb-0">Login</h1>
+            </div>
+            <div className="card-body">
+              <form onSubmit={handleLogin}>
+                <div className="mb-3">
+                  <label className="form-label" htmlFor="email">
+                    Email address
+                  </label>
+                  <input
+                    type="email"
+                    className="form-control"
+                    name="email"
+                    id="email"
+                    aria-describedby="emailHelp"
+                    required
+                  />
                 </div>
+                <div className="mb-3">
+                  <label className="form-label" htmlFor="password">
+                    Password
+                  </label>
+                  <input
+                    type="password"
+                    className="form-control"
+                    name="password"
+                    id="password"
+                    required
+                  />
+                </div>
+                <button type="submit" className="btn btn-primary">
+                  Login
+                </button>
+              </form>
+            </div>
+            <div className="card-footer">
+              <p className="text-muted">
+                Don't have an account?{' '}
+                <Link to="/register" className="text-decoration-none">
+                  Register
+                </Link>
+              </p>
+            </div>
+          </div>
         </div>
-    )
+      </div>
+
+      {email && <Profile email={email} />}
+    </div>
+  );
 }
 
 export default Login;
--- src/Profile.js
+++ src/Profile.js
@@ -2,87 +2,39 @@ import React from 'react';
 import Navbar from './Navbar';
 import { Link } from 'react-router-dom';
 import Chart from './Chart';
-function Profile() {
+
+function Profile(props) {
   return (
     <div>
-        <Navbar />
-        <main role="main" class="container">
-        <h1>User's Knowledge Level</h1>
-
+      <Navbar />
+      <main role="main" class="container">
+        <h1>{props.email}'s Knowledge Level</h1>
 
         <div class="container">
-        <div class="row text-white rounded">
-          
-        <div class="col-md-5 blog-sidebar mt-4"><Chart />
-          </div>
-
-
-
-              <div class="col">
-            <div class="card flex-md-row mb-4 box-shadow h-md-250">
-              <div class="card-body d-flex flex-column align-items-start">
-                <strong class="d-inline-block mb-2 text-success "><h1>Your Financial Goal</h1></strong>
-                <h3>What do you see yourself in 5 years?</h3>
-                <input class="form-control form-control-lg" type="text" placeholder=".form-control-lg"></input>
-                <br></br>
-                <h3>What is your financial goal?</h3>
-                <input class="form-control form-control-lg" type="text" placeholder=".form-control-lg"></input>
-                <br></br>
-
-                <h3>How do you want yo pay off your debts?</h3>
-                <input class="form-control form-control-lg" type="text" placeholder=".form-control-lg"></input>
-                <br></br>
-                <h3>How do plan to invest for long-term?</h3>
-                <input class="form-control form-control-lg" type="text" placeholder=".form-control-lg"></input>
-              </div>
-              {/* <img class="card-img-right flex-auto d-none d-md-block" data-src="holder.js/200x250?theme=thumb" alt="Card image cap"> */}
+          <div class="row text-white rounded">
+            <div class="col-md-5 blog-sidebar mt-4"><Chart />
             </div>
-          </div>
-
-
-
-
-
-
-
+            <div class="col">
+              <div class="card flex-md-row mb-4 box-shadow h-md-250">
+                <div class="card-body d-flex flex-column align-items-start">
+                  <strong class="d-inline-block mb-2 text-success "><h1>Your Financial Goal</h1></strong>
+                  <h3>What do you see yourself in 5 years?</h3>
+                  <input class="form-control form-control-lg" type="text" placeholder=".form-control-lg"></input>
+                  <br></br>
+                  <h3>What is your financial goal?</h3>
+                  <input class="form-control form-control-lg" type="text" placeholder=".form-control-lg"></input>
+                  <br></br>
+                  <h3>How do you want to pay off your debts?</h3>
+                  <input class="form-control form-control-lg" type="text" placeholder=".form-control-lg"></input>
+                  <br></br>
+                  <h3>How do plan to invest for long-term?</h3>
+                  <input class="form-control form-control-lg" type="text" placeholder=".form-control-lg"></input>
+                </div>
+              </div>
+            </div>
           </div> 
         </div>
-
-
-
-
-        {/* <div class="col-md-4 blog-sidebar mt-4">
-          <Chart />
-          </div>
-
-          <div class="col-md-6">
-            <div class="card flex-md-row mb-4 box-shadow h-md-250">
-              <div class="card-body d-flex flex-column align-items-start">
-                <strong class="d-inline-block mb-2 text-success ">Concept of the Day</strong>
-                <h3 class="mb-0">
-                  <a class="text-dark" href="#">
-                    Moral Hazard
-                  </a>
-                </h3>
-                <div class="mb-1 text-muted">Nov 11</div>
-                <p class="card-text mb-auto">
-                Moral hazard is a situation in which one party gets involved in a risky event knowing that it is protected against the risk and the other party will incur the cost.
-                </p>
-                <a href="#">Continue reading</a>
-              </div>
-              <img class="card-img-right flex-auto d-none d-md-block" data-src="holder.js/200x250?theme=thumb" alt="Card image cap">
-            </div>
-          </div> */}
-
-
-
-
-
-
-
-        </main>
-        
-
+      </main>
     </div>
   )
 }
