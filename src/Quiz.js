1930a1a8e44768b2a83f5e3e24fc17190bb091ce
Anshuman Sahu <anshuman2601@gmail.com>
2023-04-29 18:09:39 -0500

    Updated Quiz & Study pages

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,72 +1,76 @@
-import React,{useState} from 'react'
-import Navbar from './Navbar'
-import {Link} from "react-router-dom"
+import React, { useState } from "react";
+import Navbar from "./Navbar";
+import { Link } from "react-router-dom";
 export const quiz = {
-  topic: 'Javascript',
-  level: 'Beginner',
+  topic: "Javascript",
+  level: "Beginner",
   totalQuestions: 4,
   perQuestionScore: 5,
   questions: [
     {
-      question:
-        '1. Being on a budget means:',
-      choices: ['a. You pay bills every month at the due date', 'b. You made a plan of your expenses to be less than or equal to your income ', 'c. You are earning enough money to be able to live well', 'd. Your bills are generally paid by every due date'],
-      type: 'MCQs',
-      correctAnswer: 'b. You made a plan of your expenses to be less than or equal to your income ',
-    },
-    {
-      question:
-        '2. What is considered an excellent credit score?',
-      choices: ['a. 700 to 850 ', 'b. 500 to 560', 'c. 561 to 600', 'd. 1000 to 1500'],
-      type: 'MCQs',
-      correctAnswer: 'a. 700 to 850 ',
-    },
-    {
-      question:
-        '3. Which is a method to fund your college education?',
+      question: "1. Being on a budget means:",
       choices: [
-        'a. Apply for financial aid using the FAFSA and TAP applications',
-        'b. Seek out less expensive college alternatives',
-        'c. Seek out college scholarships',
-        'd. All of the above',
+        "a. You pay bills every month at the due date",
+        "b. You made a plan of your expenses to be less than or equal to your income ",
+        "c. You are earning enough money to be able to live well",
+        "d. Your bills are generally paid by every due date",
       ],
-      type: 'MCQs',
-      correctAnswer: 'd. All of the above',
+    
+      correctAnswer:
+        "b. You made a plan of your expenses to be less than or equal to your income ",
     },
     {
-      question: '4. What is not a method to improve your credit score?',
-      choices: ['a. Pay all your bills on time', 'b. Maintain your older credit cards', 'c. Keep your credit card balances under 30% of your credit limit', 'd. Always pay in cash and do not get any credit cards'],
-      type: 'MCQs',
-      correctAnswer: 'd. Always pay in cash and do not get any credit cards',
+      question: "2. What is considered an excellent credit score?",
+      choices: [
+        "a. 700 to 850 ",
+        "b. 500 to 560",
+        "c. 561 to 600",
+        "d. 1000 to 1500",
+      ],
+      
+      correctAnswer: "a. 700 to 850 ",
+    },
+    {
+      question: "3. Which is a method to fund your college education?",
+      choices: [
+        "a. Apply for financial aid using the FAFSA and TAP applications",
+        "b. Seek out less expensive college alternatives",
+        "c. Seek out college scholarships",
+        "d. All of the above",
+      ],
+      
+      correctAnswer: "d. All of the above",
+    },
+    {
+      question: "4. What is not a method to improve your credit score?",
+      choices: [
+        "a. Pay all your bills on time",
+        "b. Maintain your older credit cards",
+        "c. Keep your credit card balances under 30% of your credit limit",
+        "d. Always pay in cash and do not get any credit cards",
+      ],
+      
+      correctAnswer: "d. Always pay in cash and do not get any credit cards",
     },
   ],
-}
-
-
-
-
-
-
-
-
-
+};
 
 const Quiz = () => {
-  const [activeQuestion, setActiveQuestion] = useState(0)
-  const [selectedAnswer, setSelectedAnswer] = useState('')
-  const [showResult, setShowResult] = useState(false)
-  const [selectedAnswerIndex, setSelectedAnswerIndex] = useState(null)
+  const [activeQuestion, setActiveQuestion] = useState(0);
+  const [selectedAnswer, setSelectedAnswer] = useState("");
+  const [showResult, setShowResult] = useState(false);
+  const [selectedAnswerIndex, setSelectedAnswerIndex] = useState(null);
   const [result, setResult] = useState({
     score: 0,
     correctAnswers: 0,
     wrongAnswers: 0,
-  })
+  });
 
-  const { questions } = quiz
-  const { question, choices, correctAnswer } = questions[activeQuestion]
+  const { questions } = quiz;
+  const { question, choices, correctAnswer } = questions[activeQuestion];
 
   const onClickNext = () => {
-    setSelectedAnswerIndex(null)
+    setSelectedAnswerIndex(null);
     setResult((prev) =>
       selectedAnswer
         ? {
@@ -75,154 +79,108 @@ const Quiz = () => {
             correctAnswers: prev.correctAnswers + 1,
           }
         : { ...prev, wrongAnswers: prev.wrongAnswers + 1 }
-    )
+    );
     if (activeQuestion !== questions.length - 1) {
-      setActiveQuestion((prev) => prev + 1)
+      setActiveQuestion((prev) => prev + 1);
     } else {
-      setActiveQuestion(0)
-      setShowResult(true)
+      setActiveQuestion(0);
+      setShowResult(true);
     }
-  }
+  };
 
   const onAnswerSelected = (answer, index) => {
-    setSelectedAnswerIndex(index)
+    setSelectedAnswerIndex(index);
     if (answer === correctAnswer) {
-      setSelectedAnswer(true)
+      setSelectedAnswer(true);
     } else {
-      setSelectedAnswer(false)
+      setSelectedAnswer(false);
     }
-  }
+  };
 
-  const addLeadingZero = (number) => (number > 9 ? number : `0${number}`)
-
-
-
-
-
-
-
-
-  
-
-
-
-
-
+  const addLeadingZero = (number) => (number > 9 ? number : `0${number}`);
 
   return (
     <div>
       <Navbar />
       <main role="main" class="container">
-
-
-      <div className="container">
-            <div className="row">
-                <div className="col-md-6 mx-auto mt-5">
-                    <div className="card">
-                        <div className="card-header">
-                            <h1 className="h3 mb-0">
-                            <div>
-            <span className="active-question-no">
-              {addLeadingZero(activeQuestion + 1)}
-            </span>
-            <span className="total-question">
-              /{addLeadingZero(questions.length)}
-            </span>
-          </div>
-
-
-
-                            </h1>
-                        </div>
-                        <div className="card-body">
-                            
-                        <div className="quiz-container">
-      {!showResult ? (
-        <div>
-
-          <h2>{question}</h2>
-          
-          
-          <ul class="list-group list-group-light">
-            {choices.map((answer, index) => (
-              <li class="list-group-item"
-                onClick={() => onAnswerSelected(answer, index)}
-                key={answer}
-                className={
-                  selectedAnswerIndex === index ? 'selected-answer' : null
-                }>
-                  
-                {answer}
-              </li>
-            ))}
-          </ul>
-          <div className="flex-right">
-            <button
-              onClick={onClickNext}
-              disabled={selectedAnswerIndex === null}>
-              {activeQuestion === questions.length - 1 ? 'Finish' : 'Next'}
-            </button>
-          </div>
-        </div>
-
-
-
-      ) : (
-        <div className="result">
-          <h3>Result</h3>
-          <p>
-            Total Question: <span>{questions.length}</span>
-          </p>
-          <p>
-            Total Score:<span> {result.score}</span>
-          </p>
-          <p>
-            Correct Answers:<span> {result.correctAnswers}</span>
-          </p>
-          <p>
-            Wrong Answers:<span> {result.wrongAnswers}</span>
-          </p>
-        </div>
-      )}
-    </div>
-
-
-                        </div>
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6 mx-auto mt-5">
+              <div className="card">
+                <div className="card-header">
+                  <h1 className="h3 mb-0">
+                    <div>
+                      <span className="active-question-no">
+                        {addLeadingZero(activeQuestion + 1)}
+                      </span>
+                      <span className="total-question">
+                        /{addLeadingZero(questions.length)}
+                      </span>
                     </div>
+                  </h1>
                 </div>
+                <div className="card-body">
+                  <div className="quiz-container">
+                    {!showResult ? (
+                      <div>
+                        <h2>{question}</h2>
+
+                        <div class="list-group list-group-light">
+                          {choices.map((answer, index) => (
+                            <div key={index}>
+                              <label>
+                            <input 
+                              type="radio"
+                              name="answer"
+                              value={answer}
+                              onClick={() => onAnswerSelected(answer, index)}
+                              checked={selectedAnswerIndex === index}
+                            />
+                            {answer}
+                          </label><br /><br />
+                            </div>
+                          ))}
+                        </div>
+
+                        <br />
+                        <div className="flex-right">
+                          <button
+                            className="btn btn-primary"
+                            onClick={onClickNext}
+                            disabled={selectedAnswerIndex === null}
+                          >
+                            {activeQuestion === questions.length - 1
+                              ? "Finish"
+                              : "Next"}
+                          </button>
+                        </div>
+                      </div>
+                    ) : (
+                      <div className="result">
+                        <h3>Result</h3>
+                        <p>
+                          Total Question: <span>{questions.length}</span>
+                        </p>
+                        <p>
+                          Total Score:<span> {result.score}</span>
+                        </p>
+                        <p>
+                          Correct Answers:<span> {result.correctAnswers}</span>
+                        </p>
+                        <p>
+                          Wrong Answers:<span> {result.wrongAnswers}</span>
+                        </p>
+                      </div>
+                    )}
+                  </div>
+                </div>
+              </div>
             </div>
+          </div>
         </div>
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
-    </main>
+      </main>
     </div>
-  )
-}
+  );
+};
 
-export default Quiz
+export default Quiz;
